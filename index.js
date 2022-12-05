59de33a4486c3c5a3d94e4ce6c4514e30cdab7fb
Christian Ani <iamchristianani@gmail.com>
2022-12-05 09:54:08 +0100

    seperate the files to be moved to modules

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
 
 const form = document.querySelector('form');
-const bookInfo = document.querySelector('.all-books');
-const title = document.querySelector('#input-title');
-const author = document.querySelector('#input-author');
 const dateEl = document.querySelector('.date');
 const listBtn = document.querySelector('.list-button');
 const addBtn = document.querySelector('.add-button');
@@ -15,61 +12,66 @@ const listAnchor = document.querySelector('.list-anchor');
 const addAnchor = document.querySelector('.add-anchor');
 const contactAnchor = document.querySelector('.contact-anchor');
 
-let bookArr = [];
+//IMPORT MODULES HERE
 
-class Book {
-  constructor(title, author, id) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
-  }
+import {Book, showBook} from './modules/bookDisplay.js';
 
-  static bookDisplay() {
-    bookInfo.innerHTML = '';
-    for (let i = 0; i < bookArr.length; i += 1) {
-      const oneBook = document.createElement('div');
-      oneBook.className = 'book';
-      oneBook.innerHTML = `
-        <p class="title">"${bookArr[i].title}" by ${bookArr[i].author}</p>
-        <button class="remove" onclick="Book.removeBook(${i})">Remove</button>
-      `;
-      bookInfo.appendChild(oneBook);
-      title.value = '';
-      author.value = '';
-    }
-  }
+// let bookArr = [];
 
-  static addBook() {
-    const eachBook = {};
-    eachBook.id = bookArr.length;
-    eachBook.title = title.value;
-    eachBook.author = author.value;
+// class Book {
+//   constructor(title, author, id) {
+//     this.title = title;
+//     this.author = author;
+//     this.id = id;
+//   }
 
-    bookArr.push(eachBook);
-    Book.bookDisplay();
+//   static bookDisplay() {
+//     bookInfo.innerHTML = '';
+//     for (let i = 0; i < bookArr.length; i += 1) {
+//       const oneBook = document.createElement('div');
+//       oneBook.className = 'book';
+//       oneBook.innerHTML = `
+//         <p class="title">"${bookArr[i].title}" by ${bookArr[i].author}</p>
+//         <button class="remove" onclick="Book.removeBook(${i})">Remove</button>
+//       `;
+//       bookInfo.appendChild(oneBook);
+//       title.value = '';
+//       author.value = '';
+//     }
+//   }
 
-    const jsonData = JSON.stringify(bookArr);
-    localStorage.setItem('form', jsonData);
-  }
+//   static addBook() {
+//     const eachBook = {};
+//     eachBook.id = bookArr.length;
+//     eachBook.title = title.value;
+//     eachBook.author = author.value;
 
-  static removeBook(index) {
-    bookArr.splice(index, 1);
-    Book.bookDisplay();
-    const jsonData = JSON.stringify(bookArr);
-    localStorage.setItem('form', jsonData);
-  }
-}
+//     bookArr.push(eachBook);
+//     Book.bookDisplay();
+
+//     const jsonData = JSON.stringify(bookArr);
+//     localStorage.setItem('form', jsonData);
+//   }
+
+//   static removeBook(index) {
+//     bookArr.splice(index, 1);
+//     Book.bookDisplay();
+//     const jsonData = JSON.stringify(bookArr);
+//     localStorage.setItem('form', jsonData);
+//   }
+// }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   Book.addBook();
 });
 window.addEventListener('load', () => {
-  const getJsonData = localStorage.getItem('form');
-  if (getJsonData) {
-    bookArr = JSON.parse(getJsonData);
-  }
-  Book.bookDisplay();
+  // const getJsonData = localStorage.getItem('form');
+  // if (getJsonData) {
+  //   bookArr = JSON.parse(getJsonData);
+  // }
+  // Book.bookDisplay();
+  showBook();
 });
 
 // SPA Navigation
