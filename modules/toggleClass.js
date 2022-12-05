812da1a3e4850394ba072768d23b4cc1ba73c4b1
Christian Ani <iamchristianani@gmail.com>
2022-12-05 14:00:39 +0100

    Add all toggle functions to a module and export to index file

---
+++ modules/toggleClass.js
@@ -0,0 +1,42 @@
+import {bookSection, addBookSection, contactSection, listAnchor, addAnchor, contactAnchor} from './variableList.js';
+
+const showBookList = () => {
+  bookSection.classList.add('active');
+  bookSection.classList.remove('hiiden');
+  addBookSection.classList.add('hidden');
+  addBookSection.classList.remove('active');
+  contactSection.classList.add('hidden');
+  contactSection.classList.remove('active');
+
+  listAnchor.classList.add('list-active');
+  addAnchor.classList.remove('list-active');
+  contactAnchor.classList.remove('list-active');
+}
+
+const showAddBook = () => {
+  bookSection.classList.remove('active');
+  bookSection.classList.add('hidden');
+  addBookSection.classList.add('active');
+  addBookSection.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+  contactSection.classList.remove('active');
+
+  listAnchor.classList.remove('list-active');
+  addAnchor.classList.add('list-active');
+  contactAnchor.classList.remove('list-active');
+}
+
+const showRemoveBook = () => {
+  bookSection.classList.add('hidden');
+  bookSection.classList.remove('active');
+  addBookSection.classList.remove('active');
+  addBookSection.classList.add('hidden');
+  contactSection.classList.add('active');
+  contactSection.classList.remove('hidden');
+
+  listAnchor.classList.remove('list-active');
+  addAnchor.classList.remove('list-active');
+  contactAnchor.classList.add('list-active');
+}
+
+export {showBookList, showAddBook, showRemoveBook};
